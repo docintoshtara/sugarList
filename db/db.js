18b6c2e34271444b7ad9f0a36224d07d90011e25
Taramaurya <tara.ecolods@gmail.com>
2024-08-21 11:09:49 +0530

    remove error

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(DBCON)
+mongoose.connect(process.env.DBCON)
     .then((res) => res)
     .then((conn) => { console.log("db connected") })
     .catch((e) => { console.log(e) })
