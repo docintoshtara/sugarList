6d6043afaeb25185ebb054c067a6ccf52dfa7831
Taramaurya <tara.ecolods@gmail.com>
2024-08-21 11:07:45 +0530

    addmongoInenv

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://root:Tdq3HaISqglHjclh@cluster0.xeyux4e.mongodb.net/sugerList?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(DBCON)
     .then((res) => res)
     .then((conn) => { console.log("db connected") })
     .catch((e) => { console.log(e) })
